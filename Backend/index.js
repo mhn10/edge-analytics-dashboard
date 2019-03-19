8411c026e99c969de3b4cb74a80fbf5735792f60
Mithun Harikumar Nair <mithun.harikumar10@gmail.com>
2019-03-18 21:03:55 -0700

    Upload and task working with connection to rabbit

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,16 @@
 //import the require dependencies
-require('dotenv').config();
+require("dotenv").config();
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-const AWS = require('aws-sdk');
-const fs = require('fs');
-const fileType = require('file-type');
-const bluebird = require('bluebird');
-const multiparty = require('multiparty');
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const fileType = require("file-type");
+const bluebird = require("bluebird");
+const multiparty = require("multiparty");
 
 const mongoose = require("mongoose");
 var cors = require("cors");
-
 
 // configure the keys for accessing AWS
 AWS.config.update({
@@ -25,13 +24,11 @@ AWS.config.setPromisesDependency(bluebird);
 // create S3 instance
 const s3 = new AWS.S3();
 
-
-
 // abstracts function to upload a file returning a promise
 const uploadFile = (buffer, name, type) => {
   console.log("Uploading file to s3");
   const params = {
-    ACL: 'public-read',
+    ACL: "public-read",
     Body: buffer,
     Bucket: process.env.S3_BUCKET,
     ContentType: type.mime,
@@ -40,7 +37,6 @@ const uploadFile = (buffer, name, type) => {
   console.log("params: ", params);
   return s3.upload(params).promise();
 };
-
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -93,47 +89,88 @@ app.get("/healthcheck", (req, res) => {
   res.send();
 });
 
-
 // Define POST route
-app.post('/uploaddata', (request, response) => {
+app.post("/uploaddata", (request, response) => {
   console.log("tEst upload data", process.env.AWS_ACCESS_KEY_ID);
   const form = new multiparty.Form();
-    form.parse(request, async (error, fields, files) => {
-      if (error) throw new Error(error);
-      try {
-        const path = files.file[0].path;
-        const buffer = fs.readFileSync(path);
-        const type = fileType(buffer);
-        const timestamp = Date.now().toString();
-        const fileName = `user1/data/${timestamp}-lg`;
-        const data = await uploadFile(buffer, fileName, type);
-        return response.status(200).send(data);
-      } catch (error) {
-        return response.status(400).send(error);
-      }
-    });
+  form.parse(request, async (error, fields, files) => {
+    if (error) throw new Error(error);
+    try {
+      const path = files.file[0].path;
+      const buffer = fs.readFileSync(path);
+      const type = fileType(buffer);
+      const timestamp = Date.now().toString();
+      const fileName = `Data/001/${timestamp}-lg`;
+      const data = await uploadFile(buffer, fileName, type);
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+  });
 });
 
-
-app.post('/uploadmodel', (request, response) => {
+app.post("/uploadmodel", (request, response) => {
   console.log("tEst upload model", process.env.AWS_ACCESS_KEY_ID);
   const form = new multiparty.Form();
-    form.parse(request, async (error, fields, files) => {
-      if (error) throw new Error(error);
-      try {
-        const path = files.file[0].path;
-        const buffer = fs.readFileSync(path);
-        const type = fileType(buffer);
-        const timestamp = Date.now().toString();
-        const fileName = `user1/model/${timestamp}-lg`;
-        const data = await uploadFile(buffer, fileName, type);
-        return response.status(200).send(data);
-      } catch (error) {
-        return response.status(400).send(error);
-      }
-    });
+  form.parse(request, async (error, fields, files) => {
+    if (error) throw new Error(error);
+    try {
+      const path = files.file[0].path;
+      const buffer = fs.readFileSync(path);
+      const type = fileType(buffer);
+      const timestamp = Date.now().toString();
+      const fileName = `Models/001/${timestamp}-lg`;
+      const data = await uploadFile(buffer, fileName, type);
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+  });
 });
 
+// Define POST route
+app.get("/output", (req, res) => {
+  console.log("get output data", process.env.S3_BUCKET);
+  // var params = {Bucket: process.env.S3_BUCKET, Key: 'user1/data/1552948993601-lg.png'};
+  // s3.getSignedUrl('putObject', params, function (err, url) {
+  //   console.log('The URL is', url);
+  // });
+
+  s3.listObjects({ Bucket: process.env.S3_BUCKET, Prefix:'Results/' }, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
+    } else {
+      console.log("success", data);
+      let keys = data.Contents.map(eachKey => eachKey.Key);
+      console.log("Keys array map", keys);
+      res.status(200);
+      res.send({ data: keys });
+      // "https://jetsons3test.s3.amazonaws.com"+data.key
+    }
+  });
+});
+
+var amqp = require("amqplib/callback_api");
+app.post("/sendtask", (request, response) => {
+  console.log("req: ", request.data);
+
+  amqp.connect("amqp://jetson:jetson@73.92.205.30:5672", function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      var q = "task_queue";
+      console.log("Process argv ", process.argv);
+      var msg = "001 IRIS.csv model";
+
+      ch.assertQueue(q, { durable: true });
+      ch.sendToQueue(q, Buffer.from(msg), { persistent: true });
+      console.log(" [x] Sent '%s'", msg);
+      return response.status(200).send(msg);
+    });
+    setTimeout(function() {
+      conn.close();
+    }, 500);
+  });
+});
 
 app.listen(3001);
 console.log("Server Listening on port 3001");
--- Frontend/src/components/home.jsx
+++ Frontend/src/components/home.jsx
@@ -1,87 +1,279 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Navbar from './Navbar/navbar';
+import React, { Component } from "react";
+import axios from "axios";
+import Navbar from "./Navbar/navbar";
+import styled from "styled-components";
+import Button from '@material-ui/core/Button';
+import Input from "@material-ui/core/Input";
 
 class FileUpload extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       file: null,
       model: null,
       data: null,
-      results: ''
+      modelLocation: "",
+      dataLocation: "",
+      output: ""
     };
     this.valueChangeHandler = this.valueChangeHandler.bind(this);
+    this.getOutput = this.getOutput.bind(this);
+
     this.handleFileUpload = this.handleFileUpload.bind(this);
   }
-  valueChangeHandler = (e) => {
-
-      this.setState({ [e.target.name]: e.target.value });
-      console.log(this.state);
-    
- 
-       
-    
-}
-  submitFile = (event) => {
+  valueChangeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
+  submitFile = event => {
     event.preventDefault();
     const formData = new FormData();
     const formModel = new FormData();
 
-    formData.append('file', this.state.data[0]);
-    formModel.append('file', this.state.model[0])
-    axios.post(`http://localhost:3001/uploaddata`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    }).then(response => {
-      console.log("data upload successful", response)
-      axios.post(`http://localhost:3001/uploadmodel`, formModel, {
+    formData.append("file", this.state.data[0]);
+    formModel.append("file", this.state.model[0]);
+console.log("state before uplaod", this.state);
+    console.log("formdata oss : ", formData);
+    axios
+      .post(`http://localhost:3001/uploaddata`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          "Content-Type": "multipart/form-data"
         }
-      }).then(res=>{
-        this.setState({results : res.data.Location})
-        console.log("uploads of data successfull ")
       })
+      .then(response => {
+        //nested calls
+        console.log("data upload successful", response);
+        this.setState({ dataLocation: response.data.Location });
 
+        axios
+          .post(`http://localhost:3001/uploadmodel`, formModel, {
+            headers: {
+              "Content-Type": "multipart/form-data"
+            }
+          })
+          .then(res => {
+            this.setState({ modelLocation: res.data.Location });
+            console.log("uploads of model successfull ");
+          })
+          .catch(err => {
+            console.log("Failed to upload model");
+          });
+        // handle your response;
+      })
+      .catch(error => {
+        console.log("Failed to upload data");
+        // handle your error
+      });
+  };
 
+  handleFileUpload = event => {
 
-
-      
-      // handle your response;
-    }).catch(error => {
-      console.log("error");
-      // handle your error
-    });
-  }
-
-  handleFileUpload = (event) => {
-    this.setState({[event.target.name]: event.target.files});
+    this.setState({ [event.target.name]: event.target.files });
     console.log(this.state);
-  }
-//username
-//modelname
-//datafile
-//type of user
+  };
 
-  render () {
+
+  sendTask = event => {
+    event.preventDefault();
+   
+    axios
+      .post(`http://localhost:3001/sendtask`, {data : "user1"})
+      .then(response => {
+        //nested calls
+        console.log("data upload successful", response);
+        
+      })
+      .catch(error => {
+        console.log("Failed to send task");
+        // handle your error
+      });
+  };
+  
+  getOutput = event => {
+    axios.get("http://localhost:3001/output").then(response => {
+      console.log("Fetched output = ", response.data.data);
+      this.setState({ output: response.data.data });
+    });
+    console.log("get output");
+  };
+  //username
+  //modelname
+  //datafile
+  //type of user
+
+  render() {
+    var allImgs = Array.prototype.slice.call(this.state.output);
     return (
       <React.Fragment>
+        <Navbar />
+        <body>
+          <form onSubmit={this.submitFile} style={{ marginLeft: "100px", marginTop:"150px" }}>
+            <input
+              name="model"
+              label="upload model"
+              type="file"
+              onChange={this.handleFileUpload}
+              accept=".csv"
+            />
+            <Input
+              name="data"
+              label="upload datafile"
+              type="file"
+              onChange={this.handleFileUpload}
+            />
+            <Button type="submit">Send</Button>
+            
+            <dl className="form-row">
+            <dl className="col-6">
+            <div className="card" style={{
+                      marginLeft: "3rem",
+                      marginRight: "3rem",
+                      marginBottom: "1rem",
+                      width:"40vh",
 
-        <Navbar/>
-        <form onSubmit={this.submitFile} style={{margin:"100px"}}>
-        <input name='model'  label='upload model' type='file' onChange={this.handleFileUpload} />
-        <input name='data'  label='upload datafile' type='file' onChange={this.handleFileUpload} />
-        <button type='submit'>Send</button>
-        <a href={this.state.results}> Location</a>
-      </form>
+                      padding: "1.5rem",
+                      //  marginLeft: "3rem",
+                      borderRadius: "7px",
+                      backgroundColor: "#f0f0f0",
+                      boxShadow:
+                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                    }}>
+            <a href={this.state.modelLocation}>Model Location</a>
+            </div>
+            </dl>
+            <dl className="col-6">
+            <div className="card col-3" style={{
+                      marginLeft: "3rem",
+                      marginRight: "3rem",
+                      marginBottom: "1rem",
+                      width:"40vh",
+                      padding: "1.5rem",
+                      //  marginLeft: "3rem",
+                      borderRadius: "7px",
+                      backgroundColor: "#f0f0f0",
+                      boxShadow:
+                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                    }}>
+            <a href={this.state.dataLocation}> Data Location</a>
+            </div>
+            </dl>
 
-        </React.Fragment>
+            </dl>
+            
+            
+            
+           
+          </form>
+          <Button type="submit" onClick={this.sendTask} style={{ marginLeft: "100px", marginTop:"40px" }}>Send Task</Button>
+          <Button color="primary"
+            style={{ marginLeft: "100px", marginTop:"40px" }}
+            type="submit"
+            onClick={this.getOutput}
+          >
+            Get Output
+          </Button>
+          <ul className="card" style={{width: "70vh", display:"block", marginLeft: "45px"}}>
+            {allImgs.map((file, i) => (
+              <a href={"https://cmpe295-jetson-s3.s3.amazonaws.com/" + file}>
+                <li class="cards__item"   id={i}>
+                  <div
+                    className="card"
+                    style={{
+                      marginLeft: "3rem",
+                      marginRight: "3rem",
+                      marginBottom: "1rem",
 
-
+                      padding: "1.5rem",
+                      //  marginLeft: "3rem",
+                      borderRadius: "7px",
+                      backgroundColor: "#f0f0f0",
+                      boxShadow:
+                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                    }}
+                  
+                  >
+                    <p class="card__text">{file}</p>
+                  </div>
+                </li>
+                {console.log("file:", file, "Key: ", i)}
+              </a>
+            ))}
+          </ul>
+        </body>
+      </React.Fragment>
     );
   }
 }
 
 export default FileUpload;
+
+const body = styled.body`
+  color: @gray;
+  font-family: "Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  letter-spacing: 0;
+  padding: 1rem;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -moz-font-feature-settings: "liga" on;
+`;
+
+const btn = styled.button`
+  background-color: #ccc;
+  border: 2px solid @gray-light;
+  border-radius: 5px;
+  color: @gray-dark;
+  padding: 0.5rem;
+  text-transform: lowercase;
+`;
+
+const btnBlock = styled(btn)`
+  display: block;
+  width: 100%;
+`;
+
+const cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const cardsitem = styled.div`
+  display: flex;
+  padding: 1rem;
+`;
+
+const card = styled(cards)`
+  background-color: white;
+  border-radius: 0.25rem;
+  box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+const card__content = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  padding: 1rem;
+`;
+
+const card__title = styled.div`
+  color: @gray-dark;
+  font-size: 1.25rem;
+  font-weight: 300;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+
+const card__text = styled.div`
+  flex: 1 1 auto;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  margin-bottom: 1.25rem;
+`;
