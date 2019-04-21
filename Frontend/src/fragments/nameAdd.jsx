df9945b001e9b5954ce0dcf6c41469144f1bb2db
Mithun Harikumar Nair <mithun.harikumar10@gmail.com>
2019-04-21 10:03:15 -0700

    Converting Task Name fragment to react select

--- Frontend/src/fragments/nameAdd.jsx
+++ Frontend/src/fragments/nameAdd.jsx
@@ -3,66 +3,73 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import AddContext from "../context/addContext";
 import styled from "styled-components";
+import CreatableSelect from "react-select/lib/Creatable";
+import axios from 'axios';
 
 const NamesAdd = props => {
- 
-  const context = React.useContext(AddContext);
+    const context = React.useContext(AddContext);
 
-  
-const changeHandler = values => {
-  console.log("Values, ", values );
-  
-  context.dispatch({ type: "setName", values });
 
-  
-  
-};
-  const submitHandler = values => {
+    useEffect(async () => {
+      console.log("fetch data here");
+      //setData();
 
-    context.dispatch({ type: "changeState", value: 3 });
-  };
-  return (
-    <NameWrapper>
+  }, []);
 
-        <Form onSubmit={e => submitHandler(e)}>
-          <Form.Group className="row">
-            <Form.Label>Name of Task</Form.Label>
-            <Form.Control    type="text" placeholder={context.addState.name}   onChange={e => changeHandler(e.target.value)} />
-            <Form.Text className="text-muted">
-              Must be unique identifier
-            </Form.Text>
-          </Form.Group>
 
-          
-          <Button variant="primary" type="submit" disabled={context.addState.name=== ''? true : false}>
-            Next
-          </Button>
-        </Form>
+    const changeHandler = values => {
+        console.log("Values, ", values);
 
-    </ NameWrapper>
-  );
+        context.dispatch({ type: "setName", values });
+    };
+    const submitHandler = values => {
+        context.dispatch({ type: "changeState", value: 3 });
+    };
+    return (
+        <NameWrapper>
+            <Form onSubmit={e => submitHandler(e)}>
+                <Form.Group className="row">
+                    <Form.Label>Name of Task</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder={context.addState.name}
+                        onChange={e => changeHandler(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                        Must be unique identifier
+                    </Form.Text>
+                </Form.Group>
+
+                <Button
+                    variant="primary"
+                    type="submit"
+                    disabled={context.addState.name === "" ? true : false}
+                >
+                    Next
+                </Button>
+            </Form>
+        </NameWrapper>
+    );
 };
 
 export default NamesAdd;
 
-
-
 const NameWrapper = styled.div`
-  background: white;
-  border: 2px solid #f8f8f8;
-  margin-top: 2rem;
-  padding: 2rem;
-  box-sizing: border-box;
-  border-radius: 10px;
-  color: #606060;
-  /* font-size: 1.2rem; */
-  text-align: center;
-
-  .item-name {
-    font-size: 1rem;
-    /* text-transform: uppercase; */
-    color: #008fda;
-    font-weight: lighter;
+    background: white;
+    border: 2px solid #f8f8f8;
+    margin-top: 2rem;
+    padding: 2rem;
+    box-sizing: border-box;
+    border-radius: 10px;
+    color: #606060;
+    /* font-size: 1.2rem; */
     text-align: center;
-  }
+
+    .item-name {
+        font-size: 1rem;
+        /* text-transform: uppercase; */
+        color: #008fda;
+        font-weight: lighter;
+        text-align: center;
+    }
 `;
