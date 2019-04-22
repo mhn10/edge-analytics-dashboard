aebc59eab929a1dfc43fb35d83a674ce498e3881
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-04-21 19:51:00 -0700

    update for queue message

--- Jetson/worker.py
+++ Jetson/worker.py
@@ -1,6 +1,7 @@
 import boto3
 import subprocess
 import sys, time, os
+import json
 
 # Get service resource
 sqs = boto3.resource( 'sqs' )
@@ -11,21 +12,13 @@ queue = sqs.get_queue_by_name( QueueName='taskQ1' )
 try:
     while True:
         # Process messages by printing out body and optional author name
-        for message in queue.receive_messages( MessageAttributeNames = [ 'User', 'Type' ] ):
-            # Get user name and action name
-            user = ''
-            action = ''
-            if message.message_attributes is not None:
-                user = message.message_attributes.get( 'User' ).get( 'StringValue' )
-                action = message.message_attributes.get( 'Type' ).get( 'StringValue' )
-                # if user:
-                #     user_name = ' ({0})'.format( user )
-                # if action:
-                #     action_name = ' ({0})'.format( action )
+        for message in queue.receive_messages():
+
+            js = json.loads(message.body)
 
             # Print out body and author
-            print('user: {0}, Name: {1}, Type: {2}'.format( user, message.body, action ) )
-            subprocess.Popen( [ 'python3', 'bucket.py', user, action, message.body ] )
+            print('Name: {0} '.format( js ) )
+            # subprocess.Popen( [ 'python3', 'bucket.py', user, action, message.body ] )
             # Let the queue know that the message is processed
             message.delete()
 except KeyboardInterrupt:
