3cabed625ffb6f49773cde04cba02c3b08b28a59
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-04-24 23:36:46 -0700

    Add JS code for nodeDetail

--- Backend/Mqtt/mqtt_sub.js
+++ Backend/Mqtt/mqtt_sub.js
@@ -1,83 +1,88 @@
-const mqtt = require('mqtt');
-// require("../dotenv").config();
-// var broker = 'mqtt://iot.eclipse.org:1883'
-var broker = 'broker.hivemq.com:1883'
-// const client = mqtt.connect(process.env.MQTT_BROKER);
-const client = mqtt.connect(broker);
-// const {client} = require('../index');
-var allNodes = [];
-var nodeDetail ={};
+// const mqtt = require('mqtt');
+// // require("../dotenv").config();
+// // var broker = 'mqtt://iot.eclipse.org:1883'
+// var broker = 'mqtt://54.67.84.242:1883'
+// // const client = mqtt.connect(process.env.MQTT_BROKER);
+// const client = mqtt.connect(broker);
+// // const {client} = require('../index');
+// var allNodes = [];
+// var nodeDetail ={};
 
-// console.log( 'Connected to broker at: %s', process.env.MQTT_BROKER )
-console.log( 'Connected to broker at: %s', broker );
+// // console.log( 'Connected to broker at: %s', process.env.MQTT_BROKER )
+// console.log( 'Connected to broker at: %s', broker );
 
-var topics = ['NodeInfo', 'ActiveNodes', 'TaskResults'];
+// var topics = ['NodeInfo', 'ActiveNodes', 'TaskResults'];
 
-client.on('connect', () => {
-	for( var i = 0; i < topics.length; i++ )
-	{
-		client.subscribe( topics[i] )
-		console.log( 'Subscribed to: %s', topics[i] );
-	}
-})
+// client.on('connect', () => {
+// 	for( var i = 0; i < topics.length; i++ )
+// 	{
+// 		client.subscribe( topics[i] )
+// 		console.log( 'Subscribed to: %s', topics[i] );
+// 	}
+// })
 
-client.on('message', (topic, message) => {
-	switch( topic ) {
-		case 'NodeInfo':
-			return handleNodeInfo( message )
-		case 'ActiveNodes':
-			return handleActiveNodes( message )
-		case 'TaskResults':
-			return handleResults( message )
-	}
-	console.log( 'No handler for this topic %s', topic )
-})
+// client.on('message', async (topic, message) => {
+// 	switch( topic ) {
+// 		case 'NodeInfo':
+// 			return await handleNodeInfo( message )
+// 		case 'ActiveNodes':
+// 		console.log("Message received for Active Nodes");
+// 			return await handleActiveNodes( message )
+// 		case 'TaskResults':
+// 			return await handleResults( message )
+// 	}
+// 	console.log( 'No handler for this topic %s', topic )
+// })
 
-function handleNodeInfo( message ) {
-	console.log( 'Message received:', message);
-	nodeDetail = strMapToObj(message);
-}
+// function handleNodeInfo( message ) {
+// 	console.log( 'Message received:', message);
+// 	nodeDetail = strMapToObj(message);
+// }
 
-function handleActiveNodes( message ) {
-	allNodes = [];
-	js = JSON.parse( message );
-	for ( i in js['Active'] ) {
-		// console.log(allNodes);
-		// console.log( js['Active'][i] );
-		var NodeInfoMap = new Map();
-		for ( key in js['Active'][i] ) {
-			// console.log(key);
-			NodeInfoMap.set( key, js['Active'][i][key] );
-		}
-		// console.log("i: ", NodeInfoMap);
-		var obj = strMapToObj(NodeInfoMap);
-		allNodes.push( obj );
-		// console.log("allNode: ", allNodes);
-	}
-	// console.log( 'Message received for Active Node %s', message );
-}
+// function handleActiveNodes( message ) {
+// 	console.log("Message",message);
+// 	// allNodes = [];
+// 	js = JSON.parse( message );
+// 	for ( i in js['Active'] ) {
+// 		// console.log(allNodes);
+// 		// console.log( js['Active'][i] );
+// 		var NodeInfoMap = new Map();
+// 		for ( key in js['Active'][i] ) {
+// 			// console.log(key);
+// 			NodeInfoMap.set( key, js['Active'][i][key] );
+// 		}
+// 		console.log("i: ", NodeInfoMap);
+// 		var obj = strMapToObj(NodeInfoMap);
+// 		allNodes.push( obj );
+// 		// return allNodes;
+// 		// console.log("allNode: ", allNodes);
+// 	}
+// 	// console.log( 'Message received for Active Node %s', message );
+// }
 
-function handleResults( message ) {
-	console.log("Inside handle Result, message received", message);
-}
+// function handleResults( message ) {
+// 	console.log("Inside handle Result, message received", message);
+// }
 
-function getNodesInfo() {
-	console.log("Inside getNodesInfo");
-	console.log("NodeInfoMap", allNodes);
-	return allNodes;
-}
+// function  getNodesInfo() {
+// 	console.log("Inside getNodesInfo");
+// 	while(allNodes.length === 0){
+// 		continue;
+// 	}
+// 	return allNodes;
+// }
 
-function getNodeDetail() {
-	console.log("Inside getNodeDetail");
-	console.log("NodeDetail", nodeDetail);
-	return nodeDetail;
-}
-module.exports = {getNodesInfo, getNodeDetail};
+// function getNodeDetail() {
+// 	console.log("Inside getNodeDetail");
+// 	console.log("NodeDetail", nodeDetail);
+// 	return nodeDetail;
+// }
+// module.exports = {getNodesInfo, getNodeDetail};
 
-function strMapToObj(temp) {
-	let obj = Object.create(null);
-	for (let [k,v] of temp) {
-		obj[k] = v;
-	}
-	return obj;
-}
+// function strMapToObj(temp) {
+// 	let obj = Object.create(null);
+// 	for (let [k,v] of temp) {
+// 		obj[k] = v;
+// 	}
+// 	return obj;
+// }
--- Backend/routes/nodes/nodeDetail.js
+++ Backend/routes/nodes/nodeDetail.js
@@ -1,30 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const AWS = require("aws-sdk");
-const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-const {getNodeDetail} = require('../../Mqtt/mqtt_sub');
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+// const { getNodeDetail } = require('../../Mqtt/mqtt_sub');
+const axios = require('axios');
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	console.log("Inside Node Detail API");
 	var nodeId = req.body.nodeid;
 	// create parameters
 	var data = {
-		nodeId : nodeId,
-		action: "Info" 
+		nodeId: nodeId,
+		action: "Info"
 	}
 	var dataTobeSent = JSON.stringify(data);
 	var params = {
 		MessageBody: dataTobeSent, // Its the name given by user for their upload
 		QueueUrl: `${process.env.QUEUE_URL}NodeCommunication` // URL of our queue
 	};
-	sqs.sendMessage(params, (err, data) => {
+	await sqs.sendMessage(params, async (err, data) => {
 		if (err) {
 			console.log("Error", err);
 			return res.status(400).send(err);
 		} else {
-			// console.log("Success", data.MessageId);
-			var result = getNodeDetail();
-			return res.status(200).send(result);
+			try {
+				let result = await axios.get(`${process.env.PYTHON_BACKEND}node`);
+				let data = await result.data;
+				return res.status(200).send(data);
+			}
+			catch (e) {
+				console.log(e);
+			}
 		}
 	});
 });
