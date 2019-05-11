56bfdb1461277cafa011995bcc67a76376ec3dd5
Mithun Harikumar Nair <mithun.harikumar10@gmail.com>
2019-05-11 01:13:32 -0700

    Updated the Deploy loading

--- Frontend/src/Constants.js
+++ Frontend/src/Constants.js
@@ -1,10 +1,8 @@
 export const CONSTANTS = {
   ROOTURL: "http://localhost:3000",
-  BACKEND_URL: "http://localhost:3001",
-  NODE_STATUS: "http://localhost:5000",
+  BACKEND_URL: "http://ec2-54-215-156-177.us-west-1.compute.amazonaws.com:3001",
+  NODE_STATUS: "http://ec2-54-215-156-177.us-west-1.compute.amazonaws.com:5000",
   REACT_APP_GOOGLE_API_KEY : "AIzaSyBFz3lNKEE4cNtgdYB5XNSaI7Nwx4v7vhE" 
 };
 
 
-// BACKEND_URL: "http://ec2-54-215-156-177.us-west-1.compute.amazonaws.com:3001",
-// NODE_STATUS: "http://ec2-54-215-156-177.us-west-1.compute.amazonaws.com:5000",
--- Frontend/src/components/nodeStatus.jsx
+++ Frontend/src/components/nodeStatus.jsx
@@ -1,11 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import { css } from '@emotion/core';
-import { DotLoader } from 'react-spinners';
+import { css } from "@emotion/core";
+import { RingLoader } from "react-spinners";
+import ReactEventSource from "react-eventsource";
 const { CONSTANTS } = require("../Constants");
-
-
 
 const override = css`
     display: block;
@@ -14,45 +13,51 @@ const override = css`
 `;
 
 const NodeStatus = props => {
-
-
+    // const eventStreamer = () => {
+    //     console.log("fetch Task here");
+    //     const eventsource = new EventSource('http://localhost:5000/demo');
+    //     eventsource.onmessage = e => {
+    //         console.log("e data ", e.data);
+    //     };
+    // };
 
     // useEffect(() => {
     //     console.log("fetch Task here");
-    //     axios
-    //         .get(`${CONSTANTS.NODE_STATUS}/updates/kaikai/task7`,{headers: {
-    //             'Access-Control-Allow-Origin': '*',
-    //             'Content-Type': 'application/json',
-    //           }})
-    //         .then(response => {
-    //             console.log("Response taskdetails", response);
-    //             //create option map to setDeafultoption
-               
-    //         })
-    //         .catch(error => {
-    //             console.log("Error in useEffect nameAdd", error);
-    //             alert("Reload page");
-    //         });
+    //     eventStreamer();
+    //     // axios
+    //     //     .get(`${CONSTANTS.NODE_STATUS}/demo`, {
+    //     //         headers: {
+    //     //             "Mime-Type": "text/plain",
+    //     //             "Content-Type": "text/event-stream"
+    //     //         }
+    //     //     })
+    //     //     .then(response => {
+    //     //         console.log("Response taskdetails", response);
+    //     //         //create option map to setDeafultoption
+    //     //     })
+    //     //     .catch(error => {
+    //     //         console.log("Error in useEffect nameAdd", error);
+    //     //         alert("Reload page");
+    //     //     });
     // }, []);
 
+    // const renderEvent = event => <div>{ event }</div>
     return (
-
-
-        <div style={{textAlign:"left"}}>
-
-            Loader Comp 
-            <DotLoader
-          css={override}
-          sizeUnit={"px"}
-          size={60}
-          color={'#007bff'}
-          loading={true}
-        />
-          </div>  
-    )
-
-
-
-}
+        <div style={{ textAlign: "left" }}>
+            {/* <ReactEventSource url="http://localhost:5000/demo">
+        
+              {events => events.map(renderEvent) }
+            </ReactEventSource>
+            Loader Comp */}
+            <RingLoader
+                css={override}
+                sizeUnit={"px"}
+                size={30}
+                color={"#1edbed"}
+                loading={true}
+            />
+        </div>
+    );
+};
 
 export default NodeStatus;
